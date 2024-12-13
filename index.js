9949427bc3a4e9836d7530317d3c18a99c4c7507
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-12-13 21:24:32 +0300

    8.5

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors:  [Author!]!
   }
 `
@@ -109,10 +109,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (_, args) => {
-      if(args.author) {
-        return books.filter(book => book.author === args.author)
+      console.log(args)
+      let filteredBooks = books;
+      if (args.author) {
+        filteredBooks = filteredBooks.filter((book) => book.author === args.author)
       }
-      return books
+
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((book) => book.genres.includes(args.genre))
+      }
+      
+      return filteredBooks
     },
     allAuthors: () => authors,
   },
