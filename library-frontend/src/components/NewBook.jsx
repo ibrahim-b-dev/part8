f1141967c6a0ed1080cdc6f40c225f0936d01cd1
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-12-30 13:24:19 +0300

    8.10

--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useMutation } from "@apollo/client"
+import { useState } from "react"
+import { ALL_BOOKS, CREATE_BOOK } from "../queries"
 
 const NewBook = (props) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [published, setPublished] = useState('')
-  const [genre, setGenre] = useState('')
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [published, setPublished] = useState("")
+  const [genre, setGenre] = useState("")
   const [genres, setGenres] = useState([])
+
+  const [createBook, { data, loading, error }] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }],
+  })
 
   if (!props.show) {
     return null
@@ -14,18 +20,22 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    createBook({
+      variables: { title, published: parseInt(published), author, genres },
+    })
+    console.log(published)
+    console.log("add book...")
 
-    setTitle('')
-    setPublished('')
-    setAuthor('')
+    setTitle("")
+    setPublished("")
+    setAuthor("")
     setGenres([])
-    setGenre('')
+    setGenre("")
   }
 
   const addGenre = () => {
     setGenres(genres.concat(genre))
-    setGenre('')
+    setGenre("")
   }
 
   return (
@@ -62,7 +72,7 @@ const NewBook = (props) => {
             add genre
           </button>
         </div>
-        <div>genres: {genres.join(' ')}</div>
+        <div>genres: {genres.join(" ")}</div>
         <button type="submit">create book</button>
       </form>
     </div>
--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -20,3 +20,30 @@ export const ALL_BOOKS = gql`
     }
   }
 `
+
+// createBook is the mutation operation name
+// $title, $author, $published and $genres is a variables of type String!
+
+// addBook is the mutation being executed, used to add a new person to the system.
+// It accepts and set it's arguments with the values that passed from named mutation
+
+export const CREATE_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
